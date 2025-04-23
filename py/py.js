998f17ac60dc9cdad78fa112dca5915176935cb4
GoldComeHK <moksurky2@gmail.com>
2025-04-23 11:03:52 +0800

    Add files via upload

--- py/py.js
+++ py/py.js
@@ -108,7 +108,7 @@ def _自動獲取香港勞工處工作資料(keyword=''):
     # 转换为带换行的字符串（每条记录占一行）
     真all_Boss料_print到html = f"{現在時間}<br>" + "<br>".join(真all_Boss料) + "<br>---------<br>"  # 最后加两个换行保证分隔
 
-    搵客鍠_driver.execute_script(
+    driver.execute_script(
     """
     const textarea = document.getElementById('搵客鍠結果');
     // 标准化换行符并确保开头有分隔
