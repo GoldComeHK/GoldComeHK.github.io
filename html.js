886acb6bb7e66d36004d545c4824f05fedd3ffa1
GoldComeHK <moksurky2@gmail.com>
2025-04-27 09:47:03 +0800

    Add files via upload

---
+++ html.js
@@ -0,0 +1,32 @@
+
+
+
+
+導航=`
+    <div class="container" style="padding: 20px;background: #00000000;">
+        <div class="按鈕AA" >
+            <a href="./1.html" class='按鈕圖標' title="🔍 搵客鍠：按地區、行業、關鍵字，全天候自動為您搵新客戶。" >🔍</a> 
+            <samp>搵客鍠</samp>
+        </div>
+        <div class="按鈕AA" >
+            <a href="https://64071181.github.io/mk/" class='按鈕圖標' title="✒️ 創作鍠：按行業、文化、語氣、受眾，產出具吸引力文章、海報、影片。" >✒️</a> 
+            <samp>創作鍠</samp>
+        </div>
+        <div class="按鈕AA" >
+            <a href="./3.html" class='按鈕圖標' title="📣 促銷鍠：365日自動向潛在客戶發送促銷內容、優惠、邀約，提升轉化率。" >📣</a> 
+            <samp>促銷鍠</samp>
+        </div>
+        <div class="按鈕AA" >
+            <a href="./4.html" class='按鈕圖標' title="🫱🏾‍🫲🏻 客服鍠：7 × 24 不間斷即時回應客戶查詢，增加專業感與成交率。支援多語言。" >🫱🏾‍🫲🏻</a> 
+            <samp>客服鍠</samp>
+        </div>
+
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?si=uwBbKqm1vo_ZfsFE&amp;list=PLyJPqAgyFi65hvH3RKDUtqRb_ILBgRLN_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+`
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('導航').innerHTML = 導航; // 正确写法;
+});
+
